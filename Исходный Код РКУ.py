68f8f19b002f6a8cba04107ec485a7499bbcb9cd
XayLoad <55952514+XayLoad@users.noreply.github.com>
2019-09-29 19:46:39 +0400

    Update Исходный Код РКУ.py

--- Исходный Код РКУ.py
+++ Исходный Код РКУ.py
@@ -24,9 +24,14 @@ print("Корень D = ", disc2)
 x1 = -b + disc // 2 * a
 x2 = -b - disc // 2 * a
 
+x12 = -b + disc2 // 2 * a
+x22 = -b - disc2 // 2 * a
+
 if disc > 0:
     print("X1 = ", x1)
     print("X2 =", x2)
+    print("X1 через корень = ", x12)
+    print("X2 через корень=", x22)
 
 elif disc == 0:
     x = -b / (2 * a)
